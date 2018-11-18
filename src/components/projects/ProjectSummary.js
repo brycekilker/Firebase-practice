6307c70a3611696e4dacf2becd8c46934e097be5
Bryce Kilker <brycekilker@gmail.com>
2018-11-18 10:45:47 -0700

    progress, all componenets created

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+    return (
+        <div className="project-list section">
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Bryce</p>
+                    <p className="grey-text">Nov 15th 2am</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectSummary
